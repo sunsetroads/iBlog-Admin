f4bb95c2c5202b537f92b75f93894b4a2db407d7
sunsetroads <ningvvy@gmail.com>
2019-11-01 19:57:54 +0800

    style: format daemon.js

--- daemon.js
+++ daemon.js
@@ -1,7 +1,5 @@
 const cluster = require('cluster');
-const numCPUs = require('os')
-    .cpus()
-    .length;
+const numCPUs = require('os').cpus().length;
 
 if (cluster.isMaster) {
     let worker;
